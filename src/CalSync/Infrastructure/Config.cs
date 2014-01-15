cfebdca1ab1f36076c0af10889d28104f40d1d97
Will Fuqua <wafuqua@gmail.com>
2014-01-15 15:39:27 -0500

    minor code cleanup

--- src/CalSync/Infrastructure/Config.cs
+++ src/CalSync/Infrastructure/Config.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Linq.Expressions;
 
 namespace CalSync.Infrastructure
 {
